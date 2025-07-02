a87eef1039d19a52218e04e97e7330a587fb4c33
Josue034 <josu23uriel@gmail.com>
2025-07-02 11:38:57 -0500

    Add files via upload

---
+++ clase de funciones.cpp
@@ -0,0 +1,40 @@
+/*
+int sumatoria(int n){
+    int S=0;
+    for(int i=1;i<=n;i++){
+        S=S+i;
+    }
+    return S;
+}
+*/
+/*
+int factorial(int n){
+    int f=1;
+    for (int i=1; i<=n; i++){
+        f=f*i;
+    }
+    return f;
+}
+*/
+/*
+#include<math.h>
+int exponente (int n){
+    int alm=0;
+    for(int i=1;i<=n;i++){
+        using std::pow;
+       alm=alm+pow(i,n+1-i);
+    }
+return alm;
+}
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int nume;
+    cin>>nume;
+    //ACA SE PONE LA FUNCION QUE QUIERAS ESCRIBIR: cout<<FUNCION X(nume);
+    return 0;
+}
+
+
+
